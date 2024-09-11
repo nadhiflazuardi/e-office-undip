3b108a92ed52534cc3b4f3cb1f6d1dc741b443f4
Nadhif Lazuardi <berkasnadhif@gmail.com>
2024-09-11 14:47:45 +0700

    feat: create authentication
    - create routes to login and logout
    - create LoginRequest
    - create LoginController
    - create login and logout methods in LoginController

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home', [
+        'title' => 'Dashboard',
+    ]);
+})
+    ->middleware('auth')
+    ->name('home');
+
+Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::post('/login', [LoginController::class, 'authenticate']);
+Route::post('/logout', [LoginController::class, 'logout'])->middleware('auth');
