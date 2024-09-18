1547e5b799bf4ca3ca76750638a0a8e56af4cae6
Nadhif Lazuardi <berkasnadhif@gmail.com>
2024-09-18 11:29:01 +0700

    feat: create CRUD backend in SuratMasukController
    - create SuratMasukController and CRUD methods
    - create SuratMasukRequest

---
+++ app/Http/Controllers/SuratMasukController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\SuratMasukRequest;
+use App\Models\SuratMasuk;
+use Illuminate\Http\Request;
+
+class SuratMasukController extends Controller
+{
+    public function index()
+    {
+        $title = 'Surat Masuk';
+        $suratMasuk = SuratMasuk::all();
+        return view('surat-masuk.index', compact('title', 'suratMasuk'));
+    }
+
+    public function create()
+    {
+        $title = 'Tambah Surat Masuk';
+        return view('surat-masuk.create', compact('title'));
+    }
+
+    public function store(SuratMasukRequest $request)
+    {
+        $surat = $request->file('file_surat');
+        $namaSurat = time() . '_' . $surat->getClientOriginalName();
+        $surat->storeAs('surat_masuk', $namaSurat, 'local');
+
+        // Simpan data surat
+        SuratMasuk::create([
+            'nomor_surat' => $request->nomor_surat,
+            'perihal' => $request->perihal,
+            'asal' => $request->asal,
+            'tujuan' => $request->tujuan,
+            'file_surat' => $namaSurat,
+            'tanggal_surat' => $request->tanggal_surat,
+        ]);
+
+        return redirect()->route('surat-masuk.index');
+    }
+
+    public function show(SuratMasuk $surat)
+    {
+        $title = 'Detail Surat Masuk';
+        return view('surat-masuk.show', compact('title', 'surat'));
+    }
+
+    public function edit(SuratMasuk $surat)
+    {
+        $title = 'Edit Surat Masuk';
+        return view('surat-masuk.edit', compact('title', 'surat'));
+    }
+
+    public function update(SuratMasukRequest $request, SuratMasuk $surat)
+    {
+        // Cek kalo ada file baru di request
+        if ($request->hasFile('file_surat')) {
+            // Hapus file lama
+            $fileLama = storage_path('app/surat_masuk/' . $surat->file_surat);
+            if (file_exists($fileLama)) {
+                unlink($fileLama); // hapus file lama
+            }
+
+            // Simpan file baru
+            $suratBaru = $request->file('file_surat');
+            $namaSuratBaru = time() . '_' . $suratBaru->getClientOriginalName();
+            $suratBaru->storeAs('surat_masuk', $namaSuratBaru, 'local');
+
+            // Update file_surat di database
+            $surat->file_surat = $namaSuratBaru;
+        }
+
+        // Update data surat lainnya
+        $surat->update([
+            'nomor_surat' => $request->nomor_surat,
+            'perihal' => $request->perihal,
+            'asal' => $request->asal,
+            'tujuan' => $request->tujuan,
+            'tanggal_surat' => $request->tanggal_surat,
+        ]);
+
+        return redirect()->route('surat-masuk.index');
+    }
+
+    public function destroy(SuratMasuk $surat)
+    {
+        $fileLama = storage_path('app/surat_masuk/' . $surat->file_surat);
+        if (file_exists($fileLama)) {
+            unlink($fileLama); // hapus file lama
+        }
+
+        $surat->delete();
+        return redirect()->route('surat-masuk.index');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,11 @@ Route::get('/rapat', [RapatController::class, 'index'])->name('rapat.index');
 Route::post('/rapat/store', [RapatController::class, 'store'])->name('rapat.store');
 Route::patch('/rapat/update/{id}', [RapatController::class, 'update'])->name('rapat.update');
 Route::delete('/rapat/destroy/{id}', [RapatController::class, 'destroy'])->name('rapat.destroy');
+
+Route::get('/surat-masuk', [SuratMasukController::class, 'index'])->name('surat-masuk.index');
+Route::post('/surat-masuk/store', [SuratMasukController::class, 'store'])->name('surat-masuk.store');
+Route::get('/surat-masuk/show/{id}', [SuratMasukController::class, 'show'])->name('surat-masuk.show');
+Route::get('/surat-masuk/edit/{id}', [SuratMasukController::class, 'edit'])->name('surat-masuk.edit');
+Route::patch('/surat-masuk/update/{id}', [SuratMasukController::class, 'update'])->name('surat-masuk.update');
+Route::delete('/surat-masuk/destroy/{id}', [SuratMasukController::class, 'destroy'])->name('surat-masuk.destroy');
+
